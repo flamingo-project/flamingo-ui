6d68b7f8956af84aabf2daa47c3af3b925e87d99
Marci <mihmar17@gmail.com>
2022-07-06 20:17:06 +0200

    Updates

---
+++ precache-manifest.a13b5e63a953e8c4b0e3b663f1284925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c295213909801fbb67d325c70cbc5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f945fd435317f799a8d",
+    "url": "./static/css/main.4bb0dcf2.chunk.css"
+  },
+  {
+    "revision": "dfc01f354c605c198b88",
+    "url": "./static/js/2.21c1ee49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.21c1ee49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f945fd435317f799a8d",
+    "url": "./static/js/main.67c8528c.chunk.js"
+  },
+  {
+    "revision": "8d5b8b890a4f28e36ba5",
+    "url": "./static/js/runtime-main.0bf05434.js"
+  }
+]);
